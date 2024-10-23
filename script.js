4d6d44c84e87d2b9b0eb429e68e4d6fb9229fc84
João Ramalho <162310518+joaoramalho2006@users.noreply.github.com>
2024-10-23 11:04:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const grafico = document.getElementById("MeuGráfico").getContext("2d") ;
+const grafico = new chart (grafico, {
+
+type: "pie", 
+data: (
+
+labels:[
+"Instagram",
+"WhatsApp",
+"Facebook",
+"WeChat",
+"TikTok",
+    "DouYin",
+    "Twitter",
+    "Telegram",
+    "FB Messenger",
+    "LINE",
+]
+
+
+
+)
+
+
+
+}
+   
+    )
