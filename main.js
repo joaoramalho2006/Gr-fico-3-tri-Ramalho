157744c44772dde9b1097b40bbac04c3c0c73cb5
João Ramalho <162310518+joaoramalho2006@users.noreply.github.com>
2024-10-30 11:02:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,60 +1,49 @@
 const grafico = document.getElementById("meuGrafico").getContext("2d");
-const meuGrafico = new Chart (grafico, {
-  type: "pie",
-  data: {
-    labels:[
-      "Instagram",
-      "WhatsApp",
-      "Facebook",
-      "WeChat",
-      "TikTok",
-      "DouYin",
-      "Twitter",
-      "Telegram",
-      "FB Messenger",
-      "LINE"
-    ],
-   datasets: [{
-   data: [
-     16.5,
-     16.1,
-     12.8,
-     12.8,
-     7.4,
-     6.6,
-     3.2,
-     2.4,
-     2.3,
-     1.7,
-],
-
-backgroundcolor:[
-"rgb(0,255,255)",
-"rgb(131,111,255)",
-"rgb(124,252,0)",
-"rgb(139,0,139)",
-"rgb(255,20,147)",
-"rgb(255,0,0)",
-"rgb(255,255,0)",
-"rgb(175,238,238)",
-"rgb(0,0,205)",
-"rgb(255,140,0)",
-
-
-
-
-
-
-
-]
-
-   }]
-
-
+const meuGrafico = new Chart(grafico, {
+    type: "pie",
+    data: {
+      labels: [
+        "Instagram", 
+        "WhatsApp", 
+        "Facebook", 
+        "WeChat", 
+        "TikTok", 
+        "DouYin", 
+        "Twitter", 
+        "Telegram", 
+        "FB Messenger", 
+        "LINE"
+      ],
+      datasets: [{
+        data: [
+          16.5, 
+          16.1, 
+          12.8, 
+          12.8, 
+          7.4, 
+          6.6, 
+          3.2, 
+          2.4, 
+          2.3, 
+          1.7
+        ],
+        backgroundColor: [
+          "red", 
+          "blue",
+          "green",
+          "#ADFF2F",
+          "#DAA520",
+          "#BC8F8F",
+          "rgb(245,222,179)",
+          "rgb(186,85,211)",
+          "rgb(255,20,147)",
+          "CadetBlue",        
+        ],
+      }
+    ]
+    }
   }
-}
-)
-
+);
 
 
 
